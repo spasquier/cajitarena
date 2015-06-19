70de92edf9b6a5fdfdd2304721660d2e767d8468
Salvador Pasquier <salvapasquier@gmail.com>
2015-06-19 12:05:18 -0600

    Agregar prueba de ejemplo para iniciar

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php header("Content-Type: text/html;charset=utf-8"); ?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Mi cajita de arena para practicar PHPUnit</title>
+</head>
+<body>
+  <h1>Hola Salvador</h1>
+  <p>Bienvenido a tu cajita de arena para practicar PHPUnit</p>
+</body>
+</html>
---
+++ src/Money.php
@@ -0,0 +1,25 @@
+<?php
+class Money
+{
+    private $amount;
+
+    public function __construct($amount)
+    {
+        if (is_numeric($amount)) {
+            $this->amount = doubleval($amount);
+        } else {
+            throw new Exception("Sólo se puede inicializar el dinero usando un "
+                . "número como parámetro.", 10101);
+        }
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function negate()
+    {
+        return new Money(-1 * $this->amount);
+    }
+}
